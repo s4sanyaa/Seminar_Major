aebae6b13774fd9a71dd5b95f1791105b442573f
s4sanyaa <s4sanya.s@gmail.com>
2025-05-05 19:22:18 +0530

    Created base class for shop system

---
+++ Assets/Prefabs/Framework/ShopSystem/ShopSystem.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu(menuName = "Shop/ShopSystem")]
+public class ShopSystem : ScriptableObject
+{
+    [SerializeField] ShopItem[] shopItems;
+
+    public ShopItem[] GetShopItems()
+    {
+        return shopItems;
+    }
+
+    public bool TryPurchase(ShopItem selectedItem)
+    {
+        return false;
+    }
+}
