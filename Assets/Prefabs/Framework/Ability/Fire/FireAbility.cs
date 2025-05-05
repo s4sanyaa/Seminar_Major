009451df17cc1dad1378e370a93751d722c33006
s4sanyaa <s4sanya.s@gmail.com>
2025-05-05 15:19:47 +0530

    Composed Scaner and Fire Ability Assets

--- Assets/Prefabs/Framework/Ability/Fire/FireAbility.cs
+++ Assets/Prefabs/Framework/Ability/Fire/FireAbility.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[CreateAssetMenu(menuName = "Ability/Fire")]
 public class FireAbility : Ability
 {
     [SerializeField] Scaner ScannerPrefab;
