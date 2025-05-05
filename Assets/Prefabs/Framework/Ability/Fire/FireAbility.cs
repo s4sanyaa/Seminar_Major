a1a91b404159ddb67fee1a1c62e6fe8b7134f2fd
s4sanyaa <s4sanya.s@gmail.com>
2025-05-05 15:33:43 +0530

    Applied Damage to the enemy with fire ability

--- Assets/Prefabs/Framework/Ability/Fire/FireAbility.cs
+++ Assets/Prefabs/Framework/Ability/Fire/FireAbility.cs
@@ -8,8 +8,9 @@ public class FireAbility : Ability
     [SerializeField] Scaner ScannerPrefab;
     [SerializeField] float fireRadius;
     [SerializeField] float fireDuration;
-
-    [SerializeField] GameObject scanVFX;
+    [SerializeField] private float damageDuration = 3f;
+    [SerializeField] private float fireDamage = 20f;
+    [SerializeField] GameObject ScanVFX;
     [SerializeField] GameObject DamageVFX;
 
     public override void ActivateAbility()
@@ -18,13 +19,43 @@ public class FireAbility : Ability
         Scaner fireScaner = Instantiate(ScannerPrefab, AbilityComp.transform);
         fireScaner.SetScanRange(fireRadius);
         fireScaner.SetScanDuration(fireDuration);
-        fireScaner.AddChildAttached(Instantiate(scanVFX).transform);
+        fireScaner.AddChildAttached(Instantiate(ScanVFX).transform);
         fireScaner.onScanDetectionUpdated += DetectionUpdate;
         fireScaner.StartScan();
     }
 
     private void DetectionUpdate(GameObject newDetection)
     {
-        Debug.Log($"Detected: {newDetection.name}");
+        ITeamInterface detectedTeamInterface = newDetection.GetComponent<ITeamInterface>();
+        if (detectedTeamInterface == null || detectedTeamInterface.GetRelationTowards(AbilityComp.gameObject) != ETeamRelation.Enemy)
+        {
+            return;
+        }
+
+        HealthComponent enemyHealthComp = newDetection.GetComponent<HealthComponent>();
+        if (enemyHealthComp == null)
+        {
+            return;
+        }
+
+        AbilityComp.StartCoroutine(ApplyDamageTo(enemyHealthComp));
+    }
+
+    private IEnumerator ApplyDamageTo(HealthComponent enemyHealthComp)
+    {
+        GameObject damageVFX = Instantiate(DamageVFX, enemyHealthComp.transform);
+        float damageRate = fireDamage / fireDuration;
+        float startTime = 0;
+        while (startTime < fireDuration && enemyHealthComp != null)
+        {
+            startTime += Time.deltaTime;
+            enemyHealthComp.changeHealth(-damageRate * Time.deltaTime, AbilityComp.gameObject);
+            yield return new WaitForEndOfFrame();
+        }
+
+        if (damageVFX != null)
+        {
+            Destroy(damageVFX);
+        }
     }
 }
